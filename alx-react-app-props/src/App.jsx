51f57cc46dfddeb980539c94d91690fcfdbe3075
Mohamed <mohamedhesham7595@gmail.com>
2025-02-23 21:12:04 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,7 @@ import UserContext from './UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
+Counter.jsx
   return (
     <UserContext.Provider value={userData}>
       <ProfilePage />
