7326bddab651cffb65e6d5ae1fa60e10079b6d77
Mohamed <mohamedhesham7595@gmail.com>
2025-03-15 12:35:22 +0200

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -9,5 +9,29 @@ function App() {
     </div>
   );
 }
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+import AddTodoForm from "./components/AddTodoForm";
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a Todo App", completed: false },
+  ]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <AddTodoForm onAdd={addTodo} />
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default App;
 
 export default App;
