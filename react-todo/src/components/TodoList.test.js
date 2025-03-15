1c642062d47f97e80ecd0723cf38ab2d3b0953ff
Mohamed <mohamedhesham7595@gmail.com>
2025-03-15 12:43:21 +0200

    Create TodoList.test.js

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,22 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "./TodoList";
+
+test("renders the todo list", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("toggles todo completion on click", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText("Delete")[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
