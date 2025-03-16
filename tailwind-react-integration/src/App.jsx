8fadc2558a954812d99e3143bb2259bf7d3d93a2
Mohamed <mohamedhesham7595@gmail.com>
2025-03-16 23:26:39 +0200

    Create App.jsx

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserProfile from './components/UserProfile'; // تأكد من المسار الصحيح
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
