401fb500912fe3bf5acd353ecbc33bed7105562d
Mohamed <mohamedhesham7595@gmail.com>
2025-03-30 13:25:43 +0200

    App.jsx

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './components/Search';
+
+const App = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <Search />
+    </div>
+  );
+};
+
+export default App;
