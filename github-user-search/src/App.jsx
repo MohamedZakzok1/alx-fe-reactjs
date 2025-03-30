afa844bddcceaebf98b0efb08e4e34dd804830a5
Mohamed <mohamedhesham7595@gmail.com>
2025-03-30 13:42:18 +0200

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import Search from './components/Search';
+import React from "react";
+import Search from "./components/Search";
 
-const App = () => {
+function App() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div className="App">
+      <h1>GitHub User Search</h1>
       <Search />
     </div>
   );
-};
+}
 
 export default App;
