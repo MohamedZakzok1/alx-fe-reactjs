2bec7353c15511aec5ca4c84a5a779153b1e446e
Mohamed <mohamedhesham7595@gmail.com>
2025-04-05 15:48:39 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`https://api.github.com/users/${username}`);
+    "https://api.github.com/search/users?q", "location", "minRepos"]
     return response.data;
   } catch (error) {
     throw new Error("User not found");
